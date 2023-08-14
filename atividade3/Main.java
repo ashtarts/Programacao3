04bb0426aa544746594caa1d984b3b945e0ad663
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2023-08-14 13:11:58 -0300

    Add files via upload

---
+++ atividade3/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número inteiro: ");
+        int numero1 = scanner.nextInt();
+
+        System.out.print("Digite o segundo número inteiro: ");
+        int numero2 = scanner.nextInt();
+
+        int soma = numero1 + numero2;
+
+        System.out.println("A soma dos números é: " + soma);
+    }
+}
