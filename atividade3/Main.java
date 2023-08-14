096a22e0b321685985f85c6fe6fa140599f8c892
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2023-08-14 13:06:02 -0300

    Add files via upload

---
+++ atividade3/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numeroInteiro = scanner.nextInt();
+        float numeroDecimal = (float) numeroInteiro;
+
+        System.out.println("O número decimal é: " + numeroDecimal);
+
+    }
+}
