5fe5fe2a66d15cc6a84367ea9ca6123c98cca7f9
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2023-08-14 13:15:37 -0300

    Add files via upload

---
+++ atividade3/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o ano de nascimento: ");
+        int anoNascimento = scanner.nextInt();
+
+        int anoAtual = java.time.Year.now().getValue();
+
+        int idade = anoAtual - anoNascimento;
+
+        System.out.println("Você tem: " + idade);
+    }
+}
