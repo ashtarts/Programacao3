ead657ecda354a803237aed0e1cb95560c1024ef
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2023-08-14 13:18:57 -0300

    Add files via upload

---
+++ atividade3/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu primeiro nome: ");
+        String primeironome = scanner.nextLine();
+
+        System.out.print("Digite seu sobrenome: ");
+        String segundonome = scanner.nextLine();
+
+        String nomeC = primeironome + " " + segundonome;
+
+        System.out.println("Seu nome completo é: " + nomeC);
+
+        scanner.close();
+    }
+}
