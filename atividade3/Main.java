6dfa30eee95dbbf03c1e5ada688af3cbb8623df3
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2023-08-14 13:20:50 -0300

    Add files via upload

---
+++ atividade3/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma sequência de números separados por espaço: ");
+        String sequencia = scanner.nextLine();
+
+        String[] numeros = sequencia.split(" ");
+
+        int quantidadeNumeros = numeros.length;
+
+        System.out.println("Foram digitados " + quantidadeNumeros + " números.");
+
+        scanner.close();
+    }
+}
