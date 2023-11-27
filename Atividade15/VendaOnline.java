a504d764371c84511ec5484ce9c1c0b702f4413e
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2023-11-27 14:05:38 -0300

    Add files via upload

---
+++ Atividade15/VendaOnline.java
@@ -0,0 +1,9 @@
+public class VendaOnline implements ModoVenda {
+    @Override
+    public void efetuarVenda(String produto, String cliente, int quantidade) {
+    }
+
+    @Override
+    public void calcularDesconto() {
+        }
+}
